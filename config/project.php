6f3192a43d79dd411fa96e49869e5ba48136b54e
SeungUkLee <lsy931106@gmail.com>
2017-10-31 00:42:53 +0900

    seeder finished

---
+++ config/project.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SeungUk
+ * Date: 2017. 10. 30.
+ * Time: 오전 4:07
+ */
+
+return [
+    'tags' => [
+        'Foo',
+        'Bar',
+        'Baz',
+        'Qux'
+    ],
+];
