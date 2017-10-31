035cb96b3e931da8f7aa741a4d44dbb59041c8b3
SeungUkLee <lsy931106@gmail.com>
2017-10-31 13:16:03 +0900

    post index & show finished

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('tags/{slug}/posts', [
+    'uses' => 'PostsController@index',
+    'as' => 'tags.posts.index',
+]);
 
 Route::resource('posts', 'PostsController');
 
